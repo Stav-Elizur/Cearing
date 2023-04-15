202059f398f0ee621fd3e3f962ebe514e88df570
tommyafek <tommyafek@gmail.com>
2023-04-15 20:23:41 +0300

    (feat): find closet image by coss diff

--- sign_writing/label_sign_images.py
+++ sign_writing/label_sign_images.py
@@ -43,6 +43,30 @@ def diff_images(image_dir):
         diff(c, d)
 
 
+def check_cosin(images_dir, filepath):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+
+    image_processed = preprocess(Image.open(os.path.join(images_dir, filepath))).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image_processed).tolist()
+
+    with open("image_encodings.json") as f:
+        data = json.loads(json.load(f))
+    max_image_diff = 0
+    max_label = None
+    # Todo: array then sort by diff {id: '*.png',diff:cos_diff}
+    for dt in data:
+        vec = np.array(dt['label']).flatten()
+        img = np.array(image_features).flatten()
+        cos_diff = diff(vec,img)
+        if max_image_diff < cos_diff:
+            max_image_diff = cos_diff
+            max_label = dt
+    print("Image id: ", max_label['id'], " Cos_Diff: ", max_image_diff)
+
+
 def diff(a, b, similar=False):
     sim = ""
     if similar is not True:
@@ -56,15 +80,16 @@ def diff(a, b, similar=False):
     # Calculate the cosine similarity
     cosine_similarity = dot_product / (magnitude_a * magnitude_b)
 
-    print("Need to be ", sim, " Similiar", cosine_similarity)
+    return cosine_similarity
 
 
 if __name__ == "__main__":
-    if not os.path.exists('images'):
-        os.makedirs('images')
+    # if not os.path.exists('images'):
+    #     os.makedirs('images')
+    #
+    # with zipfile.ZipFile('images.zip', 'r') as zip_ref:
+    #     zip_ref.extractall('images')
 
-    with zipfile.ZipFile('images.zip', 'r') as zip_ref:
-        zip_ref.extractall('images')
-
-    generate_labeling("images")
-    shutil.rmtree('images')
+    # generate_labeling("images")
+    # shutil.rmtree('images')
+    check_cosin(images_dir="images",filepath="0.png")
