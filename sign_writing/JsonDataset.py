801fc2754499c7c50a4c9b6de5d8fc4e178aa8f7
Stav Elizur <stavelizur@cs.colman.ac.il>
2023-04-15 18:42:12 +0300

    Remove some warnings

--- sign_writing/JsonDataset.py
+++ sign_writing/JsonDataset.py
@@ -1,10 +1,8 @@
 import os
 import zipfile
-import glob
 import torch.utils.data as data
 import torch
 import json
-import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
 
@@ -16,25 +14,24 @@ class JsonlDataset(data.Dataset):
         with zipfile.ZipFile('image_encodings.zip', 'r') as zip_ref:
             zip_ref.extractall('')
         with open('image_encodings.json') as f:
-            image_encodings: list = json.loads(json.load(f))
-            self.image_encodings = list(filter(lambda file_name: os.path.isfile(os.path.join(self.dir_path, file_name["id"])),
-                                               image_encodings))
+            self.image_encodings: list = json.loads(json.load(f))
+
+        self.image_encodings = list(filter(lambda image_info: os.path.isfile(os.path.join(dir_path, image_info["id"])),
+                                           self.image_encodings))
 
     def __len__(self):
         return len(self.image_encodings)
 
     def __getitem__(self, index):
         preprocess_transforms = transforms.Compose([
-            transforms.Resize((224,224)),
+            transforms.Lambda(lambda rgba_img: rgba_img.convert('RGB')),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
-
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         data_dict = self.image_encodings[index]
         # convert the data_dict to PyTorch tensors or numpy arrays
         # as needed for your model
-        image = Image.open(os.path.join(self.dir_path, data_dict["id"])).convert("RGB")
-        image_processed = preprocess_transforms(image)
-        image_tensor = image_processed
+        image_tensor = preprocess_transforms(Image.open(os.path.join(self.dir_path, data_dict["id"])))
         label_tensor = torch.tensor(data_dict["label"])
         return image_tensor, label_tensor
