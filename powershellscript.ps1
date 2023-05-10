69eae78e079e85cc03be91e24d805e5500e77491
tommyafek <tommyafek@gmail.com>
2023-05-11 01:03:38 +0300

    commit message 1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -1,7 +1,6 @@
-$commitMessage = "commit message"
 for ($i = 1; $i -le 2; $i++) {
     "BATCH_NUM=$i" | Out-File -FilePath ".env" -Append
     git add .
-    git commit -m $commitMessage
+    git commit -m "commit message $i"
     git push
 }
