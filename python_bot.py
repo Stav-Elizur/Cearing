73c010b2a47f91ee7f2687c492e76c513dfa4171
Stav Elizur <stavelizur@cs.colman.ac.il>
2023-05-13 13:04:38 +0300

    feat: python bot , audio working, send default video

--- python_bot.py
+++ python_bot.py
@@ -1,49 +1,56 @@
-import io
 import os.path
+import subprocess
 
-import telegram
-import logging
-from telegram import Update, ForceReply
+import requests
+from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes,filters
-from io import BytesIO
+from pydub import AudioSegment
 
 from spoken_to_text import spoken_to_text
+from spoken_to_text.spoken_to_text import AudioRecorder
 
-FFMPEG_PATH= 'C:\\Users\\tommy\\Downloads\\ffmpeg-master-latest-win64-gpl\\ffmpeg-master-latest-win64-gpl\\bin'
-os.environ["FFMPEG_PATH"] = FFMPEG_PATH
-from pydub import AudioSegment
+TOKEN = '5993731273:AAHnf_LvkKJO-oWdXf_O20eDvRXKBF7Bkco'
+FFMPEG_PATH = 'C:\\projects\\ffmpeg\\bin'
+os.environ["ffmpeg"] = FFMPEG_PATH
 
 AudioSegment.converter = FFMPEG_PATH
 AudioSegment.ffmpeg = FFMPEG_PATH
 
-import speech_recognition as sr
-import wave
 
-#TODO: fix audio shit, create api for the model(easy), queues ?
+# TODO: fix audio shit, create api for the model(easy), queues ?
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
     )
+
+
 async def handle_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
+    file_name = 'audio'
     voice = update.message.voice
-    file_id = voice.file_id
+    file = await voice.get_file()
 
-    new_file = await context.bot.get_file(file_id)
-    cr = await new_file.download_as_bytearray()
-    ogg_audio_b = BytesIO(cr)
-    with open("audio.ogg", "wb") as f:
-        f.write(ogg_audio_b.read())
-    # ar = spoken_to_text.AudioRecorder()
-    # text = ar.convert_to_text(os.path.join('C:\\Users\\tommy\\OneDrive\\Desktop\\Cearing',"audio.ogg"))
-    # print(text)
-    ogg_audio = AudioSegment.from_file(file="audio.ogg")
-    wav_data = ogg_audio.export(format="wav").read()
-    await update.message.reply_text("okey")
+    response = requests.get(file.file_path)
 
+    with open(f'{file_name}.oga', 'wb') as f:
+        f.write(response.content)
+
+    if os.path.exists(f'{file_name}.wav'):
+        os.remove(f'{file_name}.wav')
+
+    subprocess.run(['ffmpeg', '-i', f'{file_name}.oga', f'{file_name}.wav'])
+
+    recorder = AudioRecorder()
+    text = recorder.convert_to_text(wav_filename=f'{file_name}.wav')
+    await update.message.reply_text(text)
+
+    if not text.__contains__('Exception'):
+        await context.bot.send_video(chat_id=update.effective_chat.id,
+                                     video=open('s2m0a2a63bda69676afdc0a968924754578.mp4', 'rb'))
+    os.remove(f'{file_name}.oga')
+    os.remove(f'{file_name}.wav')
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -53,7 +60,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main():
-    application = Application.builder().token('5993731273:AAHnf_LvkKJO-oWdXf_O20eDvRXKBF7Bkco').build()
+    application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("text", echo))
     application.add_handler(MessageHandler(filters.VOICE, handle_voice_message))
--- spoken_to_text/spoken_to_text.py
+++ spoken_to_text/spoken_to_text.py
@@ -19,8 +19,8 @@ class AudioRecorder:
             s = self.recognizer.recognize_google(audio_text)
             print("Path: ", wav_filename, " Text: " + s)
         except Exception as e:
-            print("wave_path: " + wav_filename + " Exception: " + str(e))
-            s = str(e)
+            s = "wave_path: " + wav_filename + " Exception: " + str(e)
+            print(s)
         return s
 
 
