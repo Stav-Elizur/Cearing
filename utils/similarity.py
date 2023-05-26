941cb2b7cffb155f63ec3e85e5d41238b1ae68ea
tkcry123 <tkcryking2@gmail.com>
2023-05-26 15:59:04 +0300

    add changes

--- utils/similarity.py
+++ utils/similarity.py
@@ -20,11 +20,11 @@ def diff(a, b, similar=False):
     return cosine_similarity
 
 
-def check_cosin(traget_vector: Tensor, data) -> list:
+def check_cosin(traget_vector: Tensor, data,chosen_paramerer='image_embedding') -> list:
     differences = []
     for dt in data:
         # dt = { "text", "image_text_encoded", "pose_url"}
-        vec = np.array(dt['embedding_vector']).flatten()
+        vec = np.array(dt[f'{chosen_paramerer}']).flatten()
         img = np.array(traget_vector.tolist()).flatten()
         cos_diff = diff(vec, img)
         differences.append((dt['word'], dt['uid'], cos_diff))
